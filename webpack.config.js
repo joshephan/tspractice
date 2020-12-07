7b4adf0529b72012b6e14493486248e82677aea0
Sanghun Han <cto.han@1f60d.com>
2020-12-08 08:31:43 +0900

    first

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      { test: /\.(js|ts|tsx)$/, use: "ts-loader", exclude: /node_modules/ },
+    ],
+  },
+  devtool: "source-map",
+  target: "node",
+  output: { filename: "bundle.js", path: path.resolve(__dirname, "dist") },
+  resolve: {
+    extensions: [".jsx", ".ts", ".tsx", ".js"],
+    alias: { "@Types": path.resolve(__dirname, "../@types/") },
+  },
+};
